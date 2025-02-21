fc20145b5940c0ca17bb273c365fad0ef9b48a3e
Andrea Freddi <56250515+AndreaFreddiGo@users.noreply.github.com>
2025-02-21 11:17:57 +0100

    Aggiungere i file di progetto.

---
+++ U4-S2-D5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace U4_S2_D5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
